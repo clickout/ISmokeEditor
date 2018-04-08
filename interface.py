eeca199e11b4ea3455ea9a089eb2937076960807
Michal Vöröš <MichalVoros@mail.com>
2018-04-08 19:50:17 +0200

    Shiet what

---
+++ interface.py
@@ -0,0 +1,64 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+
+from unidecode import unidecode
+
+from difflib import get_close_matches
+
+import items
+
+
+
+class ISmokeLayout(GridLayout):
+    Window.size = (300, 600)
+    items_scroll_view = ObjectProperty()
+    search_text_input = ObjectProperty()
+    amount_text_input = ObjectProperty()
+    console = ObjectProperty()
+    all_items_dict = items.Item.get_all_items()
+    all_items_names = [item_name[1].name for item_name in all_items_dict.items()]
+
+    def add_item(self):
+        if self.items_scroll_view.adapter.selection:
+            item_name = unidecode(self.items_scroll_view.adapter.selection[0].text.lower())
+            selected_item = self.all_items_dict.get(item_name)
+            if self.amount_text_input.text:
+                selected_item.add(int(self.amount_text_input.text))
+                self.console.text += str(selected_item) + " - přidáno {} položek.\n".format(self.amount_text_input.text)
+            else:
+                selected_item.add()
+                self.console.text += str(selected_item) + " - přidána 1 položka.\n"
+
+    def sell_item(self):
+        if self.items_scroll_view.adapter.selection:
+            item_name = unidecode(self.items_scroll_view.adapter.selection[0].text.lower())
+            selected_item = self.all_items_dict.get(item_name)
+            if self.amount_text_input.text:
+                selected_item.sell(int(self.amount_text_input.text))
+                self.console.text += str(selected_item) + " - prodáno {} položek.\n".format(self.amount_text_input.text)
+            else:
+                selected_item.sell()
+                self.console.text += str(selected_item) + " - prodána 1 položka.\n"
+
+    def search_list(self):
+        if self.search_text_input.text:
+            text = unidecode(self.search_text_input.text.lower())
+            close_matches = get_close_matches(text, self.all_items_dict.keys(), 20, 0.3)
+            new_data = [self.all_items_dict.get(item).name for item in close_matches]
+            self.items_scroll_view.adapter.data = new_data
+            self.items_scroll_view._trigger_reset_populate()
+        else:
+            self.items_scroll_view.adapter.data = self.all_items_names
+            self.items_scroll_view._trigger_reset_populate()
+
+
+class ISmokeItemEditorApp(App):
+
+    def build(self):
+        return ISmokeLayout()
+
+
+if __name__ == '__main__':
+    ISmokeItemEditorApp().run()
---
+++ items.py
@@ -0,0 +1,97 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+from datetime import date
+
+import unidecode
+
+
+class Item(object):
+
+    @staticmethod
+    def open_spreadsheet():
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        gspread_creds = gspread.authorize(credentials)
+        spreadsheet = gspread_creds.open_by_key('1m0Vhn2eDaGDAh5XcUzUvw2LCbHlHkju_bVnWsu60kW4')
+        return spreadsheet
+
+    @classmethod
+    def get_all_items(cls):
+        spreadsheet = cls.open_spreadsheet()
+        all_items = {}
+        for index, worksheet in enumerate(spreadsheet.worksheets()):
+            items = worksheet.range(2, 1, worksheet.row_count, 4)
+            for x in range(0, len(items), 4):
+                if items[x + 3].value:
+                    all_items[unidecode.unidecode(items[x].value).lower()] = cls.get_item_from_list(items[x:x + 4],
+                                                                                                    index)
+        return all_items
+
+    @classmethod
+    def get_item_from_list(cls, list_of_cells, index_of_worksheet):
+        name = list_of_cells[0].value
+        price = list_of_cells[1].value
+        bonus = list_of_cells[2].value
+        quantity = list_of_cells[3].value
+        row = list_of_cells[0].row
+        return cls(name, price, bonus, quantity, row, index_of_worksheet)
+
+    def __init__(self, name, price, bonus, quantity, row, index_of_worksheet):
+        self.name = name
+        self.price = get_digit_from_str(price)
+        self.bonus = get_digit_from_str(bonus)
+        self.quantity = int(quantity)
+        self.row = row
+        self.index_of_worksheet = index_of_worksheet
+
+    def __repr__(self):
+        return "Item(name = {}, price = {}, bonus = {}, " \
+               "quantity = {}, row = {}, index = {})".format(self.name, self.price, self.bonus, self.quantity,
+                                                             self.row, self.index_of_worksheet)
+
+    def __str__(self):
+        return "{} - cena: {},- Kč, provize: {},- Kč, množství: {}.".format(self.name, self.price, self.bonus,
+                                                                            self.quantity)
+
+    def sell(self, number_of_sold_items=1):
+        spreadsheet = self.open_spreadsheet()
+        worksheet = spreadsheet.get_worksheet(self.index_of_worksheet)
+        if self.quantity <= 0:
+            return "Zbozi je jiz vyprodané."
+        else:
+            self.quantity -= number_of_sold_items
+            worksheet.update_cell(self.row, 4, self.quantity)
+            self.update_history_of_changes(-number_of_sold_items, worksheet)
+        return str(self) + " - prodáno položek: " + str(number_of_sold_items)
+
+    def add(self, number_of_added_items=1):
+        self.quantity += number_of_added_items
+        spreadsheet = self.open_spreadsheet()
+        worksheet = spreadsheet.get_worksheet(self.index_of_worksheet)
+        worksheet.update_cell(self.row, 4, self.quantity)
+        self.update_history_of_changes(number_of_added_items, worksheet)
+        return str(self) + " - přidano polozek: " + str(number_of_added_items)
+
+    def update_history_of_changes(self, change, worksheet):
+        index_of_last_nonempty_col = len(worksheet.row_values(1))
+        current_date = "{}.{}.".format(date.today().day, date.today().month)
+        if worksheet.cell(1, index_of_last_nonempty_col).value != current_date:
+            if index_of_last_nonempty_col == worksheet.col_count:
+                worksheet.add_cols(1)
+            index_of_last_nonempty_col += 1
+            worksheet.update_cell(1, index_of_last_nonempty_col, current_date)
+        curr_value = (int(worksheet.cell(self.row, index_of_last_nonempty_col).value)
+                      if worksheet.cell(self.row, index_of_last_nonempty_col).value.strip() else 0)
+        worksheet.update_cell(self.row, index_of_last_nonempty_col, str(curr_value + change))
+
+
+def get_digit_from_str(string):
+    number = ""
+    for letter in string:
+        if letter.isdigit():
+            number += letter
+    if number:
+        return int(number)
+    else:
+        return 0
